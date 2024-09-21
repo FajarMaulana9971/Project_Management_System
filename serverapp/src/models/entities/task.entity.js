be0f2807fca2437df1bbf5fdb016abacce51701a
Fajar <133589667+FajarMaulana9971@users.noreply.github.com>
2024-09-21 10:28:47 +0700

    Fix: adjust entity and application with ERD

--- serverapp/src/models/entities/task.entity.js
+++ serverapp/src/models/entities/task.entity.js
@@ -1,43 +1,44 @@
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
 import db from "../../configurations/database/database.configuration.js";
 
-const {DataTypes} = Sequelize;
+const { DataTypes } = Sequelize;
 
 const Task = db.define(
-    "tb_m_task",{
-        id:{
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false,
-        },
-        name:{
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate:{
-                min: 5,
-                max: 100
-            }
-        },
-        description:{
-            type: DataTypes.STRING,
-        },
-        photo: {
-            type: DataTypes.BLOB("long"),
-            allowNull: true,
-        },
-        task_progress:{
-            type: DataTypes.FLOAT,
-            allowNull: false,
-            validate: {
-                min: 0,
-                max: 100
-            }
-        },
+  "tb_m_task",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
     },
-    {
-        freezeTableName: true,
-        timestamps: false,
-    }
-)
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        min: 5,
+        max: 100,
+      },
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    photo: {
+      type: DataTypes.BLOB("long"),
+      allowNull: true,
+    },
+    task_progress: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        min: 0,
+        max: 100,
+      },
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+  },
+);
 export default Task;
--- serverapp/src/models/entities/transactional-table/user-task.transactional.js
+++ serverapp/src/models/entities/transactional-table/user-task.transactional.js
@@ -1,27 +1,29 @@
 import { Sequelize } from "sequelize";
 import db from "../../../configurations/database/database.configuration.js";
 
-const {DataTypes} = Sequelize;
+const { DataTypes } = Sequelize;
 
 const UserTask = db.define(
-    "tb_tr_user_task",{
-        id:{
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false,
-        },
-        user_id:{
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        task_id:{
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        }
-    },{
-        freezeTableName: true,
-    }
-)
+  "tb_tr_user_task",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    task_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    freezeTableName: true,
+  },
+);
 
-export default UserTask
+export default UserTask;
